c70c631b49d5746e02443e9c973dd0961aca225c
bibka <bibka@MacBook-Pro-bibka.local>
2025-10-21 20:20:20 +0300

    fix: e2e tests

--- tests/unit/sample.test.cjs
+++ tests/unit/sample.test.cjs
@@ -1,4 +1,4 @@
-const { sum } = require('../src/utils/sum.cjs');
+const { sum } = require('../../src/utils/sum.cjs');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
--- tests/unit/sum.cjs
+++ tests/unit/sum.cjs
@@ -1,4 +1,4 @@
-const { sum } = require('../src/utils/sum.cjs');
+const { sum } = require('../../src/utils/sum.cjs');
 
 test('sum util adds numbers', () => {
   expect(sum(2, 3)).toBe(5);
