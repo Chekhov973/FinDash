b1f9ad83ad9127229d5db865081ac80da99a4308
s-znam <7znam10.08@gmail.com>
2025-10-21 21:00:02 +0300

    Fix: jest fix

--- tests/unit/test_g3_5_coverage.test.js
+++ tests/unit/test_g3_5_coverage.test.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 /* global test, expect */
+/* eslint-disable no-unused-vars */
 
 // Тест для проверки G3.5 (unit test coverage)
 // Эта функция не покрыта тестом, чтобы проверить, что CI блокирует PR при покрытии ниже 80%
