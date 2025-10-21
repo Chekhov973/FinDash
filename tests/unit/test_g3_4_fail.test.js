900798e6d26049fb51850c27e8507bc0c8c601f1
s-znam <7znam10.08@gmail.com>
2025-10-21 20:48:39 +0300

    Fix: jest fix

--- tests/unit/test_g3_4_fail.test.js
+++ tests/unit/test_g3_4_fail.test.js
@@ -1,5 +1,7 @@
+/* eslint-env jest */
+
 // Тест для проверки G3.4 (unit тесты)
 // Этот тест должен упасть, чтобы проверить, что CI блокирует PR при ошибке unit теста.
-test("unit fail G3.4", () => {
-  expect(1 + 1).toBe(3);
+test('unit fail G3.4', () => {
+  expect(1 + 1).toBe(4);
 });
