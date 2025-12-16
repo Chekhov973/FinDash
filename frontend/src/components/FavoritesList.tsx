5921437bb5077d660f335a48a57112c09700bff0
V0rtexyz <409024@edu.itmo.ru>
2025-12-16 21:35:44 +0300

    fix API resp

--- frontend/src/components/FavoritesList.tsx
+++ frontend/src/components/FavoritesList.tsx
@@ -109,3 +109,6 @@ export function FavoritesList({ refreshTrigger }: FavoritesListProps) {
 
 
 
+
+
+
--- frontend/src/components/HistoryList.tsx
+++ frontend/src/components/HistoryList.tsx
@@ -108,3 +108,6 @@ export function HistoryList({ refreshTrigger }: HistoryListProps) {
 
 
 
+
+
+
